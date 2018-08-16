d6f18cd6758864670141450aa234e63e878cb081
Eleonore Bergqvist <eleonore296@gmail.com>
2018-08-16 18:05:13 +0200

    Add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+import WebshopContainer from './Containers/WebshopContainer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route path="/" component={WebshopContainer} />
+          <Route path="/login" compontent={WebshopContainer} />
+          <Route path="/logout" compontent={WebshopContainer} />
+          <Route path="/register" compontent={WebshopContainer} />
+          <Route path="/admin/login" compontent={WebshopContainer} />
+          <Route path="/admin/dashboard" compontent={WebshopContainer} />
+          <Route path="/checkout" compontent={WebshopContainer} />
+          <Route path="/mypages" compontent={WebshopContainer} />
+          <Route path="/mypages" compontent={WebshopContainer} />
+          <Route path="/products" compontent={WebshopContainer} />
+          <Route path="/product/:id" component={WebshopContainer} />
+        </Switch>
       </div>
     );
   }
---
+++ frontend/src/Containers/WebshopContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WebbshopContainer extends Component {
+  render() {
+    return (
+      <div>
+        <p>WebbshopContainer</p>
+      </div>
+    );
+  }
+}
+
+export default WebbshopContainer;
